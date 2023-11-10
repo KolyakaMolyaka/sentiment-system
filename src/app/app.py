8a893c2b245b1a2865d151c7e94a3a6d5c8bcd03
Nikolay Markeev <zz5dy6@gmail.com>
2023-11-10 11:32:13 +0300

    рефакторинг app.py

--- src/app/app.py
+++ src/app/app.py
@@ -3,22 +3,27 @@ from flask import Flask
 
 def create_app():
 	app = Flask(__name__)
-	app.config.from_prefixed_env()
 
-	from .routes.create_datasets import bp as create_datasets_bp
-	app.register_blueprint(create_datasets_bp)
-
-	# swagger
-	from .apis import api
-	api.init_app(app)
-
-	# celery
-	from src.app.ext.celery.make_celery import celery_init_app
+	"""Configurations"""
 	app.config['CELERY'] = {
 		'broker_url': 'redis://redis-db',
 		'result_backend': 'redis://redis-db',
 		'task_ignore_result': True
 	}
 	app.config.from_prefixed_env()
+
+	"""Routes"""
+	# create datasets routes
+	from .routes.create_datasets import bp as create_datasets_bp
+	app.register_blueprint(create_datasets_bp)
+
+	"""Extensions"""
+	# swagger
+	from .apis import api
+	api.init_app(app)
+
+	# celery
+	from src.app.ext.celery.make_celery import celery_init_app
 	celery_init_app(app)
+
 	return app
