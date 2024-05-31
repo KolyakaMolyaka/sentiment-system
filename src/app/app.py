100eb9db027332d1d07e4ba76df2cd445a7c1434
Nikolay Markeev <zz5dy6@gmail.com>
2024-05-31 13:46:46 +0300

    рефакторинг app.py

--- src/app/app.py
+++ src/app/app.py
@@ -1,16 +1,5 @@
 from flask import Flask
 
-# import nltk
-#
-# nltk.download('punkt')
-# nltk.download('stopwords')
-#
-# from navec import Navec
-# import wget
-#
-# url = 'https://storage.yandexcloud.net/natasha-navec/packs/navec_hudlit_v1_12B_500K_300d_100q.tar'
-# filename = wget.download(url)
-# navec = Navec.load(filename)
 
 def create_app():
 	app = Flask(__name__)
